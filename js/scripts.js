228182f8f31be5976e1da920f5088179ae2187a3
Gabriela <gcastro@anzen.com.mx>
2020-08-05 14:59:54 -0500

    feat: add dataLayer

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,10 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+const analytics = () => {
+    const elementTellMeMore = document.getElementsByClassName('btn-primary')[0];
+    elementTellMeMore.addEventListener( () =>{
+    console.log('Hola fucnionando');
+    })
+}
